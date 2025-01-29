f87340d1513d587dec1dc0bdcaec682eefb9a706
Mert Can Köymen <mertcankoymen@gmail.com>
2025-01-30 01:21:47 +0300

    Enum added for player's gun state

--- Source/TPS/Public/Characters/Player/PlayerMovementState.h
+++ Source/TPS/Public/Characters/Player/PlayerMovementState.h
@@ -8,3 +8,17 @@ enum class ECharacterState : uint8
 	ECS_JumpingState UMETA(DisplayName = "Jumping State"),
 	ECS_GravityState UMETA(DisplayName = "Gravity State")
 };
+UENUM(BlueprintType)
+enum class ECharacterIdleState : uint8
+{
+	ECS_Unequipped UMETA(DisplayName = "Unequipped State"), 
+	ECS_OneHanded UMETA(DisplayName = "One Handed State"), 
+	ECS_TwoHanded UMETA(DisplayName = "Two Handed State"), 
+};
+UENUM(BlueprintType)
+enum class ECharacterMoveState : uint8
+{
+	ECS_Unequipped UMETA(DisplayName = "Unequipped State"),
+	ECS_OneHanded UMETA(DisplayName = "One Handed State"), 
+	ECS_TwoHanded UMETA(DisplayName = "Two Handed State"), 
+};
